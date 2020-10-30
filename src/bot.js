ff9d485713ec73c807eb8fd4b59e775312d05316
mark <markpackham1@gmail.com>
2020-10-30 19:05:39 +0000

    client login test, https://youtu.be/BmKXBVdEV0g?t=1286

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,14 @@
 // Discord Bot in JS learned from https://www.youtube.com/watch?v=BmKXBVdEV0g
+// never commit the .env file with bot ids to git for security reasons
+// Discord JS documentation https://discord.js.org/#/docs/main/stable/general/welcome
 require("dotenv").config();
 
 const { Client, WebhookClient } = require("discord.js");
+
+const client = new Client();
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+// use the environment variable
+client.login(process.env.DISCORDJS_BOT_TOKEN);
