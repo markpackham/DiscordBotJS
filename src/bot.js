b91b2cfaeba1c5d94e87b97371bb363f2001228a
mark <markpackham1@gmail.com>
2020-10-31 17:46:22 +0000

    add roles, https://youtu.be/BmKXBVdEV0g?t=4274

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,10 @@
 require("dotenv").config();
 
 const { Client, WebhookClient } = require("discord.js");
-const client = new Client();
+const client = new Client({
+  // we need to use partials for data that isn't cached (eg adding roles and keeping those roles)
+  partials: ["MESSAGE", "REACTION"],
+});
 // can be any symbol other than "$"" but don't use a reserved symbol on Discord such as "@"
 const PREFIX = "$";
 
@@ -12,14 +15,14 @@ client.on("ready", () => {
   console.log(`${client.user.tag} has logged in.`);
 });
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
   // escape this function so the bot doesn't talk to itself in an infinite loop
   if (message.author.bot) return;
-  console.log(`[${message.author.tag}]: ${message.content}`);
-  if (message.content === "hello") {
-    // message.reply("hello there");
-    message.channel.send("hello");
-  }
+  // console.log(`[${message.author.tag}]: ${message.content}`);
+  // if (message.content === "hello") {
+  //   // message.reply("hello there");
+  //   message.channel.send("hello");
+  // }
   if (message.content.startsWith(PREFIX)) {
     const [CMD_NAME, ...args] = message.content
       .trim()
@@ -30,6 +33,9 @@ client.on("message", (message) => {
     // enter "$kick someUserId" in Discord to kick a user
     if (CMD_NAME === "kick") {
       //message.channel.send("Kicked the user");
+      if (!message.message.hasPermission("KICK_MEMBERS"))
+        return message.reply("You lack the power to kick members");
+
       if (args.length === 0) return message.reply("Please provide an ID");
       const member = message.guild.members.cache.get(args[0]);
 
@@ -45,6 +51,41 @@ client.on("message", (message) => {
       } else {
         message.channel.send("That member was not found");
       }
+    } else if (CMD_NAME === "ban") {
+      if (!message.member.hasPermission("BAN_MEMBERS"))
+        return message.reply("Bot lacks permissions to ban member");
+      if (args.length === 0) return message.reply("Please provide an Id");
+      try {
+        const user = await message.guild.members.ban(args[0]);
+        message.channel.send("User was banned successfully");
+      } catch (err) {
+        console.log(err);
+        message.channel.send(
+          "Error, bot either lacks permissions or the user was not found"
+        );
+      }
+    }
+  }
+});
+
+// add member roles
+client.on("messageReactionAdd", (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === "738666523408990258") {
+    switch (name) {
+      case "🍎":
+        member.roles.add("738664659103776818");
+        break;
+      case "🍌":
+        member.roles.add("738664632838782998");
+        break;
+      case "🍇":
+        member.roles.add("738664618511171634");
+        break;
+      case "🍑":
+        member.roles.add("738664590178779167");
+        break;
     }
   }
 });
